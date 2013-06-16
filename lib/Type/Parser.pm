f1d114f8629010a1d60e4fa56d15e6821b29eb72
Toby Inkster <tobyink@cpan.org>
2013-06-16 20:52:09 +0000

    Import of TOBYINK/Type-Tiny-0.007_07 from CPAN.
    gitpan-cpan-distribution: Type-Tiny
    gitpan-cpan-version:      0.007_07
    gitpan-cpan-path:         TOBYINK/Type-Tiny-0.007_07.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     developer

--- lib/Type/Parser.pm
+++ lib/Type/Parser.pm
@@ -6,7 +6,7 @@ use warnings;
 sub _croak ($;@) { require Type::Exception; goto \&Type::Exception::croak }
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.007_06';
+our $VERSION   = '0.007_07';
 
 # Token types
 # 
@@ -324,7 +324,8 @@ Tokenization: {
 	}
 	
 	{
-		package Type::Parser::Token;
+		package # hide from CPAN
+		Type::Parser::Token;
 		
 		sub is_primary
 		{
