68c90de70043476895f062d2557eaefe7e0833ec
Toby Inkster <tobyink@cpan.org>
2013-07-13 13:13:50 +0000

    Import of TOBYINK/Type-Tiny-0.015_04 from CPAN.
    gitpan-cpan-distribution: Type-Tiny
    gitpan-cpan-version:      0.015_04
    gitpan-cpan-path:         TOBYINK/Type-Tiny-0.015_04.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     developer

--- lib/Type/Tiny.pm
+++ lib/Type/Tiny.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$Type::Tiny::AUTHORITY = 'cpan:TOBYINK';
-	$Type::Tiny::VERSION   = '0.015_03';
+	$Type::Tiny::VERSION   = '0.015_04';
 }
 
 use Eval::TypeTiny ();
@@ -451,12 +451,22 @@ sub inline_check
 	my $self = shift;
 	_croak 'Cannot inline type constraint check for "%s"', $self
 		unless $self->can_be_inlined;
+	
 	return $self->parent->inline_check(@_)
 		if $self->has_parent && $self->_is_null_constraint;
 	return '(!!1)'
 		if !$self->has_parent && $self->_is_null_constraint;
-	my $r = $self->inlined->($self, @_);
-	$r =~ /[;{}]/ ? "(do { $r })" : "($r)";
+	
+	local $_ = $_[0];
+	my @r = $self->inlined->($self, @_);
+	if (@r and not defined $r[0])
+	{
+		_croak 'Inlining type constraint check for "%s" returned undef!', $self
+			unless $self->has_parent;
+		$r[0] = $self->parent->inline_check(@_);
+	}
+	my $r = join " && " => map { /[;{}]/ ? "do { $_ }" : "($_)" } @r;
+	return @r==1 ? $r : "($r)";
 }
 
 sub inline_assert
